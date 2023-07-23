856ccf53ed5f45e6179519524bd1172e464e9575
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-07-23 15:13:38 +0530

    move pino logger into common lib

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,25 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forfeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            singleLine: false,
-          },
-        },
-      },
-    }),
+    LoggerModule
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
