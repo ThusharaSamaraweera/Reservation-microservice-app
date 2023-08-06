47bda756dc2ac5d915d7bd7ef353929cdea18ca9
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-08-06 21:52:43 +0530

    add local strategy and verifyUser service

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(createUserDto: CreateUserDto) {
-    await this.userRepository.create(createUserDto);
+    await this.userRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string): Promise<any> {
+    const user = await this.userRepository.findOne({ email });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new UnauthorizedException('Credentials are not valid');
+
+    return user;
   }
 }
