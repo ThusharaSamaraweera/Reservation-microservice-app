266f24cc1e6b1c61bfdb706f07245f4c118d778b
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-08-12 21:47:50 +0530

    add jwt strategy

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -21,5 +22,9 @@ export class UsersService {
     if (!isPasswordValid) throw new UnauthorizedException('Credentials are not valid');
 
     return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.userRepository.findOne(getUserDto);
   }
 }
