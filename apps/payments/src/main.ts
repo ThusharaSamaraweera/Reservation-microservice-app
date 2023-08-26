f3bff728ff07f6468c16c1140adad8199c341046
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-08-26 15:27:50 +0530

    setup payments service

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { Transport } from '@nestjs/microservices';
+import { PAYMENT_SERVICE } from '@app/common';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: PAYMENT_SERVICE,
+      port: configService.get('PAYMENTS_HTTP_PORT'),
+    },
+  });
+  app.useLogger(app.get(Logger));
+  const port = configService.get('PAYMENTS_HTTP_PORT');
+  await app.listen(port, () => {
+    console.log(`🚀 Payment service listening at http://localhost:${port}`);
+  });
 }
 bootstrap();
--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PAYMENTS_HTTP_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
