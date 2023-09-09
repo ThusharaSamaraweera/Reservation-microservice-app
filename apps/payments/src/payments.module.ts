46ee4ba50203e8e47551748062cbabdc51d6e115
ThusharaSamaraweera <thusharadasun204@gmail.com>
2023-09-09 22:38:20 +0530

    register notification service on payment service

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
-import { LoggerModule } from '@app/common';
+import { LoggerModule, NOTIFICATION_SERVICE } from '@app/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -13,8 +14,22 @@ import { LoggerModule } from '@app/common';
       validationSchema: Joi.object({
         PAYMENTS_TCP_PORT: Joi.number().required(),
         STRIPE_SECRET_KEY: Joi.string().required(),
+        NOTIFICATIONS_TCP_PORT: Joi.number().required(),
       }),
     }),
+    ClientsModule.registerAsync([
+      {
+        name: NOTIFICATION_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: NOTIFICATION_SERVICE,
+            port: configService.get('NOTIFICATIONS_TCP_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
